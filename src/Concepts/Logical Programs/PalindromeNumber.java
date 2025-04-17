58aadac85555dc760cadb5763bbeff9fc6be3e75
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-04-17 08:46:12 +0530

    new files

---
+++ src/Concepts/Logical Programs/PalindromeNumber.java
@@ -0,0 +1,23 @@
+public class PalindromeNumber {
+  public static boolean isPalindrome(int x) {
+
+    int revNum = 0;
+
+    int dup = x;
+
+    while (x > 0) {
+      int ld = x % 10;
+      revNum = (revNum * 10) + ld;
+      x = x / 10;
+    }
+
+    if (dup == revNum) return true;
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    
+   System.out.println( isPalindrome(122));
+  }
+}
